2e0c7ff6cf8ebf60e6d82ed03aa3b409f931db57
Thamidul Hoque <thamidulhoque@gmail.com>
2026-03-11 11:29:12 +0200

    #

--- PytestPython/Playwright/test_playwrightBasics.py
+++ PytestPython/Playwright/test_playwrightBasics.py
@@ -1,18 +1,36 @@
-def test_playwright_basics(playwright):
-    # Launch the browser
-    browser = playwright.chromium.launch(headless=False)
+from playwright.sync_api import Page
 
-    context = browser.new_context()
+# def test_playwright_basics(playwright):
+#     # Launch the browser
+#     browser = playwright.chromium.launch(headless=False)
+
+#     context = browser.new_context()
     
-    # Create a new page
-    page = context.new_page()
+#     # Create a new page
+#     page = context.new_page()
     
-    # Navigate to the desired URL
-    page.goto("https://www.youtube.com")
+#     # Navigate to the desired URL
+#     page.goto("https://www.youtube.com")
     
-    # Perform actions on the page (e.g., click, fill forms, etc.)
-    # Example: Click on a link with text "More information"
-    page.click("text=More information")
+#     # Perform actions on the page (e.g., click, fill forms, etc.)
+#     # Example: Click on a link with text "More information"
+#     page.click("text=More information")
     
-    # Close the browser
-    browser.close()
+#     # Close the browser
+#     browser.close()
+
+
+# def test_playwright_basics_with_page(page: Page):
+#     # Navigate to the desired URL
+#         page.goto("https://www.google.com")
+#     # Perform actions on the page (e.g., click, fill forms, etc.)
+#     # Example: Click on a link with text "More information" 
+
+
+def test_playwright_basics_with_page(page: Page):
+        page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+        page.get_by_label("Username:").fill("rahulshettyacademy")
+        page.get_by_label("Password:").fill("learning")
+        page.get_by_role("combobox").select_option("teach")
+        page.locator("#terms").check()
+        page.get_by_role("button", name="Sign In").click()
